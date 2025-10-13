49f18a6fae3df711a920abc7ca9c068cb640fa75
Keilani Li <97659645+keil4ni@users.noreply.github.com>
2025-10-13 15:44:29 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,15 +1,33 @@
+//step 1
 console.log('hai...');
 
 function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
-navLinks = $$("nav a");
+//step 2
+// navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// currentLink?.classList.add('current'); // prevents errors w optional chaining operator
 
-currentLink?.classList.add('current');
-// prevents errors w optional chaining operator
+//step 3
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+}
+
+nav.insertAdjacentHTML('beforeend', `<a href = "${url}">${title}</a>`);
 
