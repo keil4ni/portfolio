5f01de2a1cd9d7004afa04427b4701a92a458a31
Keilani Li <97659645+keil4ni@users.noreply.github.com>
2025-10-14 16:05:10 -0700

    Update global.js

--- global.js
+++ global.js
@@ -30,29 +30,16 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-    let url = p.url;
+    // let url = p.url;
     if (!url.startsWith('http')) {
         url = BASE_PATH + url;
     }
     // alternative: url = !url.startsWith('http') ? BASE_PATH + url : url;
     
-    let title = p.title;
+    // let title = p.title;
 
-    // before: nav.insertAdjacentHTML('beforeend', `<a href = "${url}">${title}</a>`);
-    // after:
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    nav.append(a);
-
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add('current');
-    }
-
-    a.classList.toggle(
-        'current',
-        a.host === location.host && a.pathname === location.pathname,
-    );
+    // nav.insertAdjacentHTML('beforeend', `<a href = "${url}">${title}</a>`);
+    
 
 }
 
