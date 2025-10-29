394d6ba357bd68cbbf72b6da518bc7678d62d46a
Keilani Li <97659645+keil4ni@users.noreply.github.com>
2025-10-29 16:38:20 -0700

    lab 5: step 1.1-1.3

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadProj() {
     const projects = await fetchJSON('../lib/projects.json');
@@ -9,5 +10,19 @@ async function loadProj() {
     const projectsContainer = document.querySelector('.projects');
     renderProjects(projects, projectsContainer, 'h2');
 }
+
+let arcGen = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGen({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+// this is also valid:
+// let arc = d3.arc().innerRadius(0).outerRadius(50)({
+//   startAngle: 0,
+//   endAngle: 2 * Math.PI,
+// });
 
 loadProj();
