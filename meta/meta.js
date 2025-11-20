b2570c19b76c0b3eb48ba4ec467208a02823fd47
Keilani Li <97659645+keil4ni@users.noreply.github.com>
2025-11-19 18:15:47 -0800

    finished step 4 scrollytelling

--- meta/meta.js
+++ meta/meta.js
@@ -1,4 +1,5 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
 
 let xScale;
 let yScale;
@@ -328,6 +329,7 @@ function updateTooltipPosition(event) {
 
 let data = await loadData();
 let commits = processCommits(data);
+commits.sort((a, b) => d3.ascending(a.datetime, b.datetime));
 
 renderCommitInfo(data, commits);
 renderScatterPlot(data, commits);
@@ -479,6 +481,52 @@ function updateCommitStats(data, commits) {
   // rerender stats
   renderCommitInfo(data, commits);
 }
+
+d3.select('#scatter-story')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString('en', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    })},
+		I made <a href="${d.url}" target="_blank">${
+      i > 0 ? 'another awesome commit' : 'my first commit, and it was so awesome'
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+		Then I looked over all I had made, and I saw that it was very awesome.
+	`,
+  );
+
+function onStepEnter(response) {
+  // console.log(response.element.__data__.datetime);
+
+  const activeCommit = response.element.__data__;
+  const cutoffDate = activeCommit.datetime;
+
+  filteredCommits = commits.filter(d => d.datetime <= cutoffDate);
+
+  updateScatterPlot(data, filteredCommits);
+  updateFileDisplay(filteredCommits);
+  updateCommitStats(data, filteredCommits);
+}
+
+const scroller = scrollama();
+scroller
+  .setup({
+    container: '#scrolly-1',
+    step: '#scrolly-1 .step',
+  })
+  .onStepEnter(onStepEnter);
 
 document.getElementById('commit-progress').addEventListener('input', onTimeSliderChange);
 
